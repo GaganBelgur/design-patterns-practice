0f2387e58606ec924092f91c87f7f3d31653b3f8
GaganBelgur <gagan.belgur48@gmail.com>
2025-10-15 23:14:01 +0530

    Added Client class.

---
+++ client/src/main/kotlin/com/gaganbelgur/ProtoTypeClient.kt
@@ -0,0 +1,28 @@
+package com.gaganbelgur
+
+import com.gaganbelgur.com.gaganbelgur.Account
+import com.gaganbelgur.com.gaganbelgur.AccountWithoutProtoType
+
+fun main() {
+    val originalAccount = Account(
+        name = "Gagan Belgur",
+        phone = "8844662224",
+        email = "gaganbelgur@abc.com",
+        birthDate = "24/12/96"
+    )
+
+    val cloned = originalAccount.clone()
+    val copied = originalAccount.copy()
+
+    val updated = cloned.copy(email = "belgurgagan@abc.com")
+
+    println("Original: $originalAccount")
+    println("Cloned: $cloned")
+    println("Copied: $copied")
+    println("Updated: $updated")
+
+    println("Original Hashcode: ${originalAccount.hashCode()}")
+    println("Cloned  Hashcode: ${cloned.hashCode()}")
+    println("Copied  Hashcode: ${copied.hashCode()}")
+    println("Updated  Hashcode: ${updated.hashCode()}")
+}
