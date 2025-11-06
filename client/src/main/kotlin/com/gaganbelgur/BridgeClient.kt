48ebae7be7c1f2505392feda112b5e58a80d95ce
GaganBelgur <gagan.belgur48@gmail.com>
2025-11-07 00:00:46 +0530

    Enhance color classes and shape drawing methods with logging

--- client/src/main/kotlin/com/gaganbelgur/BridgeClient.kt
+++ client/src/main/kotlin/com/gaganbelgur/BridgeClient.kt
@@ -1,9 +1,14 @@
 package com.gaganbelgur
 
 import com.gaganbelgur.com.gaganbelgur.colors.BlueColor
+import com.gaganbelgur.com.gaganbelgur.colors.WhiteColor
 import com.gaganbelgur.com.gaganbelgur.shapes.CircleShape
+import com.gaganbelgur.com.gaganbelgur.shapes.SquareShape
 
 fun main() {
     val blueCircleShape = CircleShape(BlueColor())
     blueCircleShape.draw()
+
+    val whiteSquareShape = SquareShape(WhiteColor())
+    whiteSquareShape.draw()
 }
