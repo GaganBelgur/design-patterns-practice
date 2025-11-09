c4eea3b7de164580475eb75cacee8e00161d05c8
GaganBelgur <gagan.belgur48@gmail.com>
2025-11-10 00:41:46 +0530

    Integrate EndeavourGateWay and PulseGateWay with UPIPayment in BridgeClient

--- client/src/main/kotlin/com/gaganbelgur/BridgeClient.kt
+++ client/src/main/kotlin/com/gaganbelgur/BridgeClient.kt
@@ -4,6 +4,9 @@ import com.gaganbelgur.com.gaganbelgur.examples.firstexample.colors.BlueColor
 import com.gaganbelgur.com.gaganbelgur.examples.firstexample.colors.WhiteColor
 import com.gaganbelgur.com.gaganbelgur.examples.firstexample.shapes.CircleShape
 import com.gaganbelgur.com.gaganbelgur.examples.firstexample.shapes.SquareShape
+import com.gaganbelgur.com.gaganbelgur.examples.secondexample.gateways.EndeavourGateWay
+import com.gaganbelgur.com.gaganbelgur.examples.secondexample.gateways.PulseGateWay
+import com.gaganbelgur.com.gaganbelgur.examples.secondexample.payments.UPIPayment
 
 fun main() {
     val blueCircleShape = CircleShape(BlueColor())
@@ -11,4 +14,11 @@ fun main() {
 
     val whiteSquareShape = SquareShape(WhiteColor())
     whiteSquareShape.draw()
+
+    val paymentGateWay = EndeavourGateWay()
+    val payment = UPIPayment(paymentGateWay)
+    payment.makePayment("ORDER123", 2500.0)
+
+    val anotherPayment = UPIPayment(PulseGateWay())
+    anotherPayment.makePayment("ORDER456", 1500.0)
 }
