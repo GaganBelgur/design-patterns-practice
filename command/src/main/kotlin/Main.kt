44a4608420c328df33048875d97435cd4e5f3ebb
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-15 00:16:59 +0530

    Added the solutions class to verify the functionality

--- command/src/main/kotlin/Main.kt
+++ command/src/main/kotlin/Main.kt
@@ -1,16 +1,36 @@
 package com.gaganbelgur
 
+import com.gaganbelgur.noncommand.Light
+import com.gaganbelgur.refactored.AcTempDecreaseCommand
+import com.gaganbelgur.refactored.AcTempIncreaseCommand
+import com.gaganbelgur.refactored.AcTemperature
+import com.gaganbelgur.refactored.Command
+import com.gaganbelgur.refactored.LightOffCommand
+import com.gaganbelgur.refactored.LightOnCommand
+import com.gaganbelgur.refactored.RemoteControl
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val light = Light()
+    val remoteControl = RemoteControl()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val lightOnCommand = LightOnCommand(light)
+    val lightOffCommand = LightOffCommand(light)
+
+    performAction(remoteControl, lightOnCommand)
+    performAction(remoteControl, lightOffCommand)
+
+    val acTemperature = AcTemperature()
+
+    val increaseAcTemperature = AcTempIncreaseCommand(acTemperature)
+    val decreaseAcTemperature = AcTempDecreaseCommand(acTemperature)
+
+    performAction(remoteControl, increaseAcTemperature)
+    performAction(remoteControl, decreaseAcTemperature)
+}
+
+fun performAction(remoteControl: RemoteControl, command: Command) {
+    remoteControl.setCommand(command)
+    remoteControl.pressButton()
 }
