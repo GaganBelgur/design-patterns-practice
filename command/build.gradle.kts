b24503b7fa181724802b9c030baba405371d6558
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-14 23:47:41 +0530

    Added the Command design pattern module.

---
+++ command/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.repositories
+
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.gaganbelgur"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(18)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
