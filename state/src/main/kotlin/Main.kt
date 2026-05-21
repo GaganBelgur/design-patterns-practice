db681dcb2ececffb132e1867155dff8446374d52
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-21 20:45:14 +0530

    Added a new state class verification block

--- state/src/main/kotlin/Main.kt
+++ state/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package com.gaganbelgur
 
 import com.gaganbelgur.com.gaganbelgur.Order
+import com.gaganbelgur.com.gaganbelgur.OrderPlacedState
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,4 +10,11 @@ fun main() {
     println(order.getState())
     order.progressToNextState()
     println(order.getState())
+    order.cancelOrder()
+    println(order.getState())
+
+    println("Post Implementing the Stateful Objects")
+    val orderContext = OrderPlacedState()
+    orderContext.nextOrder()
+    println(orderContext.getState())
 }
