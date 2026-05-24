8fe55227c45906e1f6c3efb6a8dcb126bfef2d8a
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-24 23:25:49 +0530

    Updated the main method to invoke the new service support handlers.

--- chainofresponsibilities/src/main/kotlin/Main.kt
+++ chainofresponsibilities/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package com.gaganbelgur
 
-import com.gaganbelgur.com.gaganbelgur.SupportService
+import com.gaganbelgur.com.gaganbelgur.*
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,4 +10,16 @@ fun main() {
 
     supportService.handleSupportRequest("delivery")
     supportService.handleSupportRequest("other")
+
+    val general: SupportHandler = GeneralSupportHandler()
+    val billing: SupportHandler = BillingSupportHandler()
+    val technical: SupportHandler = TechnicalSupportHandler()
+    val delivery: SupportHandler = DeliverySupportHandler()
+
+    // Setting up the chain: general -> billing -> technical -> delivery
+    general.setNextHandler(billing)
+    billing.setNextHandler(technical)
+    technical.setNextHandler(delivery)
+
+    general.handleRequest(SupportTypes.TechnicalSupport)
 }
