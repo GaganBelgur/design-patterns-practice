809a1954a6ac0507cb9d13393adbe4a5199df3b9
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-27 23:55:14 +0530

    Added the YouTube playlist class to show the client class on the usage without iterator pattern.

---
+++ iterator/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package com.gaganbelgur
+
+import com.gaganbelgur.com.gaganbelgur.Video
+import com.gaganbelgur.com.gaganbelgur.YoutubePlaylist
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val playList = YoutubePlaylist()
+
+    playList.addVideo(Video("AI"))
+    playList.addVideo(Video("STAR"))
+
+    for(video in playList.getVideos()) {
+        println(video.title)
+    }
+}
---
+++ iterator/src/main/kotlin/com/gaganbelgur/YoutubePlaylist.kt
@@ -0,0 +1,16 @@
+package com.gaganbelgur.com.gaganbelgur
+/*
+Issues with this code
+
+1. Exposes the internal structure
+2. Tight coupling with underlying data structure
+3. No control over traversal
+4. Difficult to support multiple independent traversals
+*/
+class YoutubePlaylist {
+    private val videos = mutableListOf<Video>()
+
+    fun addVideo(video: Video) = videos.add(video)
+
+    fun getVideos() = videos
+}
