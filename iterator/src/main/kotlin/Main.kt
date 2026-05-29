f4dc22d974688fcece32635f560cf3b0ac9e7d5d
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-29 22:51:25 +0530

    Updated the Main class with Spotify playlist class.

--- iterator/src/main/kotlin/Main.kt
+++ iterator/src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 package com.gaganbelgur
 
+import com.gaganbelgur.com.gaganbelgur.SpotifyPlaylist
 import com.gaganbelgur.com.gaganbelgur.Video
-import com.gaganbelgur.com.gaganbelgur.YoutubePlaylist
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val playList = YoutubePlaylist()
+    val playList = SpotifyPlaylist()
 
     playList.addVideo(Video("AI"))
     playList.addVideo(Video("STAR"))
