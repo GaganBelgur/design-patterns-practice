bbad7a105417f9110dc87ba1a6eb9d6320eba9fc
GaganBelgur <gagan.belgur48@gmail.com>
2026-06-14 23:12:45 +0530

    Add Coffee interface and SimpleCoffee implementation

---
+++ decorator/src/main/kotlin/com/gaganbelgur/Coffee.kt
@@ -0,0 +1,6 @@
+package com.gaganbelgur.com.gaganbelgur
+
+interface Coffee {
+    fun getDescription(): String
+    fun coffeeCost(): Double
+}
---
+++ decorator/src/main/kotlin/com/gaganbelgur/SimpleCoffee.kt
@@ -0,0 +1,7 @@
+package com.gaganbelgur.com.gaganbelgur
+
+class SimpleCoffee: Coffee {
+    override fun getDescription(): String = "Simple coffee"
+
+    override fun coffeeCost(): Double = 50.0
+}
