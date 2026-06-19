cb60abb91b0cfbae06c614fdbc80abb9d77736cb
GaganBelgur <gagan.belgur48@gmail.com>
2026-06-19 23:43:51 +0530

    Implement main function to demonstrate observer pattern with YoutubeChannel and subscribers

--- observer/src/main/kotlin/Main.kt
+++ observer/src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package com.gaganbelgur
 
-fun main() {
+import com.gaganbelgur.com.gaganbelgur.channels.YoutubeChannel
+import com.gaganbelgur.com.gaganbelgur.subscribers.EmailSubscriber
+import com.gaganbelgur.com.gaganbelgur.subscribers.MobileSubscriber
+import java.net.URL
 
+fun main() {
+    val channel = YoutubeChannel("Tech Tutorials")
+
+    val mobileSubscriber = MobileSubscriber("Tech Tutorials")
+    val emailSubscriber = EmailSubscriber("Tech Tutorials")
+
+    channel.subscribe(mobileSubscriber)
+    channel.subscribe(emailSubscriber)
+
+    channel.uploadVideo(URL("https://www.youtube.com/watch?v=12345"))
 }
