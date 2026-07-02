2ba5b7574ee3df1efd2e541d5693643317a83264
GaganBelgur <gagan.belgur48@gmail.com>
2026-07-02 23:50:17 +0530

    Add BookingFacade usage in main function to initiate booking process

--- facade/src/main/kotlin/Main.kt
+++ facade/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package com.gaganbelgur
 
-fun main() {
+import com.gaganbelgur.com.gaganbelgur.BookingFacade
 
+fun main() {
+    val bookingFacade = BookingFacade()
+    bookingFacade.book("12345", 100.0, "product-001", "Booking successful!", 2)
 }
