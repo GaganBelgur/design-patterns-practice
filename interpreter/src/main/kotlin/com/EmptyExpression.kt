109a57fa2da6aaaac1ca2adef1af22329081fa71
GaganBelgur <gagan.belgur48@gmail.com>
2026-07-15 23:33:06 +0530

    Add Expression interface and EmptyExpression implementation

---
+++ interpreter/src/main/kotlin/com/EmptyExpression.kt
@@ -0,0 +1,5 @@
+package com.gaganbelgur.com
+
+class EmptyExpression: Expression {
+    override fun interpret() = 0
+}
---
+++ interpreter/src/main/kotlin/com/Expression.kt
@@ -0,0 +1,5 @@
+package com.gaganbelgur.com
+
+fun interface Expression {
+    fun interpret(): Int
+}
